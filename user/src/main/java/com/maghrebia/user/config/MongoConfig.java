2deb0a6bdca42a3cf218de53f69cc9018aa68802
boudidah ilyes <ilyes.boudidah@esprit.tn>
2025-02-16 23:51:26 +0100

    jwt token

--- user/src/main/java/com/maghrebia/user/config/MongoConfig.java
+++ user/src/main/java/com/maghrebia/user/config/MongoConfig.java
@@ -3,7 +3,7 @@ package com.maghrebia.user.config;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@EnableMongoRepositories(basePackages = "com.")
+@EnableMongoRepositories(basePackages = "com.maghrebia.user.repository")
 @EnableMongoAuditing
 public class MongoConfig {
 
