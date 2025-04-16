badb12c1e948782a1e7548f317307061ec2df070
omaromarsalhi <omar.salhi.job@gmail.com>
2025-04-16 18:57:22 +0100

    ai that generates sql query and execute them

--- authentication/src/main/java/com/maghrebia/authentication/exception/DataMismatchException.java
+++
@@ -1,7 +0,0 @@
-package com.maghrebia.authentication.exception;
-
-public class DataMismatchException extends RuntimeException {
-    public DataMismatchException(String message) {
-        super(message);
-    }
-}
--- authentication/src/main/java/com/maghrebia/authentication/exception/TokenMissingException.java
+++
@@ -1,7 +0,0 @@
-package com.maghrebia.authentication.exception;
-
-public class TokenMissingException extends RuntimeException {
-    public TokenMissingException(String message) {
-        super(message);
-    }
-}
